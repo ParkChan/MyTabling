8885f8cd7e3549b9dcf56b4328cf22ee1bb24af4
ParkChan <hydrarkk@naver.com>
2021-12-21 00:24:03 +0900

    [추가] - ProductDataSource 테스트 코드

---
+++ feature/save/src/test/java/com/chan/save/data/source/ProductDataSourceImplTest.kt
@@ -0,0 +1,33 @@
+package com.chan.save.data.source
+
+import com.chan.save.data.ProductsResponse
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ProductDataSourceImplTest {
+
+    private val tablingApi: TablingApi = mockk(relaxed = true)
+    private lateinit var productDataSourceImpl: ProductDataSourceImpl
+
+    @BeforeEach
+    fun setup() {
+        productDataSourceImpl = ProductDataSourceImpl(tablingApi)
+    }
+
+    @Test
+    fun `상품 가져오기 DataSource 기능 테스트`() = runBlocking {
+        val mockResponse: ProductsResponse = mockk(relaxed = true)
+
+        coEvery {
+            tablingApi.fetchProduct()
+        } returns mockResponse
+
+        val result = productDataSourceImpl.fetchProduct()
+
+        assertEquals(mockResponse, result)
+    }
+}
